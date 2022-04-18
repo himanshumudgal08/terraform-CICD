dfe45e622324a5f710d2656c87a22dbc3414cc99
himanshi7840 <himanshi7840@yahoo.com>
2022-04-18 13:29:05 +0000

    Updated main.tf

--- example/main.tf
+++ example/main.tf
@@ -1,16 +1,25 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.0.0"
-    }
-  }
+ provider "azurerm" {
+   features {
+     resource_group {
+       prevent_deletion_if_contains_resources = false
+     }
+   }
 }
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-}
+
+// terraform {
+//   required_providers {
+//     azurerm = {
+//       source  = "hashicorp/azurerm"
+//       version = ">=3.0.0"
+//     }
+//   }
+// }
+
+// # Configure the Microsoft Azure Provider
+// provider "azurerm" {
+//   features {}
+// }
 
 
 module "res_group" {
