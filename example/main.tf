8b1ca4fe86f11b21c420e6034712822c687e6427
himanshi7840 <himanshi7840@yahoo.com>
2022-04-18 11:53:06 +0000

    Updated main.tf

--- example/main.tf
+++ example/main.tf
@@ -8,7 +8,8 @@
 
 
 module "res_group" {
-  source                  = "git::git@gitlab.com:ot-azure/terraform/rg.git?ref=v1.1.0"
+  source                  = "OT-terraform-azure-modules/resource-group/azure"
+  version                 = "0.0.1"
   resource_group_name     = "test-demo01"
   resource_group_location = var.location
   lock_level_value        = ""
@@ -18,7 +19,8 @@ module "res_group" {
 }
 }
 module "vnet" {
-  source                      = "git::git@gitlab.com:ot-azure/terraform/virtual_network.git?ref=v1.1.0"
+  source                      = "OT-terraform-azure-modules/virtual-network/azure"
+  version                     = "0.0.2"
   resource_group_name         = module.res_group.resource_group_name
   resource_group_location     = var.location
   address_space               = ["10.0.0.0/16"]
@@ -32,7 +34,8 @@ Env = "dev"
 }
 
 module "subnet_module" {
-  source                  = "git::git@gitlab.com:ot-azure/terraform/subnet.git?ref=kritarth"
+  source                  = "OT-terraform-azure-modules/subnet/azure"
+  version                 = "0.0.2"
   resource_group_name     = module.res_group.resource_group_name
   vnet_name               = module.vnet.vnet_name
   create_delegation       = false
