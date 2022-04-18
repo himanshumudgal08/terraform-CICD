dce162edd41dee2f7fe6fd956e4e6fe9e31336cd
himanshi7840 <himanshi7840@yahoo.com>
2022-04-18 13:28:23 +0000

    Updated main.tf

--- example/main.tf
+++ example/main.tf
@@ -14,8 +14,9 @@ provider "azurerm" {
 
 
 module "res_group" {
-  source                  = "OT-terraform-azure-modules/resource-group/azure"
-  version                 = "0.0.1"
+  source                  = "git::git@gitlab.com:ot-azure/terraform/rg.git?ref=v1.1.0"
+  // source                  = "OT-terraform-azure-modules/resource-group/azure"
+  // version                 = "0.0.1"
   resource_group_name     = "test-demo01"
   resource_group_location = var.location
   lock_level_value        = ""
@@ -25,8 +26,9 @@ module "res_group" {
 }
 }
 module "vnet" {
-  source                      = "OT-terraform-azure-modules/virtual-network/azure"
-  version                     = "0.0.2"
+  source                      = "git::git@gitlab.com:ot-azure/terraform/virtual_network.git?ref=v1.1.0"
+  // source                      = "OT-terraform-azure-modules/virtual-network/azure"
+  // version                     = "0.0.2"
   resource_group_name         = module.res_group.resource_group_name
   resource_group_location     = var.location
   address_space               = ["10.0.0.0/16"]
@@ -40,8 +42,9 @@ Env = "dev"
 }
 
 module "subnet_module" {
-  source                  = "OT-terraform-azure-modules/subnet/azure"
-  version                 = "0.0.2"
+  source                  = "git::git@gitlab.com:ot-azure/terraform/subnet.git?ref=kritarth"
+  // source                  = "OT-terraform-azure-modules/subnet/azure"
+  // version                 = "0.0.2"
   resource_group_name     = module.res_group.resource_group_name
   vnet_name               = module.vnet.vnet_name
   create_delegation       = false
