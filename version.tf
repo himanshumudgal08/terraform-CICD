c019d27b4c5a7fbf02d557bd52be268999a180df
Himanshu Mudgal <99440004+himanshumudgal08@users.noreply.github.com>
2025-03-22 14:26:43 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -4,17 +4,10 @@ terraform {
       source  = "registry.terraform.io/hashicorp/azurerm"
       version = ">= 2.59.0"
     }
-  }
-
-  required_version = ">= 1.1.5"
-}
-
-
-terraform {
-  required_providers {
     tls = {
       source = "hashicorp/tls"
       version = "4.0.6"
     }
   }
+  required_version = ">= 1.1.5"
 }
